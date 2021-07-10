dabf15bccac7961fb0d58a16c8cb067a80bbd3f3
HossamEisa <39234961+HossamEisa@users.noreply.github.com>
2021-07-10 16:40:49 +0200

    sidebar menu fixed and added auto scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-jQuery.noConflict();
-
-jQuery(document).ready(function ($) {
+(function ($) {
     setTimeout(function (param) {
 
         $('.exportitemm').append(`
@@ -20,14 +18,14 @@ jQuery(document).ready(function ($) {
 
 
     if (sessionStorage.view == 'true') {
-        $('.dash-sidebar').addClass('closed')
+        $('.dash-sidebar').addClass('closed').closest('.dash-main').addClass('closed')
     } else {
-        $('.dash-sidebar').removeClass('closed')
+        $('.dash-sidebar').removeClass('closed').closest('.dash-main').removeClass('closed')
 
     }
 
     $('#desktop-toggler').click(function () {
-        $('.dash-sidebar').toggleClass('closed');
+        $('.dash-sidebar').toggleClass('closed').closest('.dash-main').toggleClass('closed');
         if ($('.dash-sidebar').hasClass('closed')) {
             sessionStorage.setItem('view', true);
         } else {
@@ -38,7 +36,6 @@ jQuery(document).ready(function ($) {
 
 
     $('#dash-toggler').on('click', function (e) {
-        console.log('asdasd');
         e.preventDefault();
         $('.dash-sidebar, .dash-sidecontent').toggleClass('open');
         $('.new-dashboard').toggleClass('overflow-hidden');
@@ -92,98 +89,80 @@ jQuery(document).ready(function ($) {
         progress_line_object.attr('style', 'width: ' + new_value + '%;').data('now-value', new_value);
     }
 
-    jQuery(document).ready(function () {
+    $('.f1 fieldset:first').fadeIn('slow');
 
-        /*
-            Fullscreen background
-        */
-        // $.backstretch("assets/img/backgrounds/1.jpg");
+    $('.f1 input[type="text"], .f1 input[type="password"], .f1 textarea').on('focus', function () {
+        $(this).removeClass('input-error');
+    });
 
-        // $('#top-navbar-1').on('shown.bs.collapse', function () {
-        //     $.backstretch("resize");
-        // });
-        // $('#top-navbar-1').on('hidden.bs.collapse', function () {
-        //     $.backstretch("resize");
-        // });
+    // next step
+    $('.f1 .btn-next').on('click', function () {
+        var parent_fieldset = $(this).parents('fieldset');
+        var next_step = true;
+        // navigation steps / progress steps
+        var current_active_step = $(this).parents('.f1').find('.f1-step.active');
+        var progress_line = $(this).parents('.f1').find('.f1-progress-line');
 
-        /*
-            Form
-        */
-        $('.f1 fieldset:first').fadeIn('slow');
-
-        $('.f1 input[type="text"], .f1 input[type="password"], .f1 textarea').on('focus', function () {
-            $(this).removeClass('input-error');
-        });
-
-        // next step
-        $('.f1 .btn-next').on('click', function () {
-            var parent_fieldset = $(this).parents('fieldset');
-            var next_step = true;
-            // navigation steps / progress steps
-            var current_active_step = $(this).parents('.f1').find('.f1-step.active');
-            var progress_line = $(this).parents('.f1').find('.f1-progress-line');
-
-            // fields validation
-            parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function () {
-                if ($(this).val() == "") {
-                    $(this).addClass('input-error');
-                    next_step = false;
-                } else {
-                    $(this).removeClass('input-error');
-                }
-            });
-            // fields validation
-
-            if (next_step) {
-                parent_fieldset.fadeOut(400, function () {
-                    // change icons
-                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
-                    // progress bar
-                    bar_progress(progress_line, 'right');
-                    // show next step
-                    $(this).next().fadeIn();
-                    // scroll window to beginning of the form
-                    scroll_to_class($('.f1'), 20);
-                });
+        // fields validation
+        parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function () {
+            if ($(this).val() == "") {
+                $(this).addClass('input-error');
+                next_step = false;
+            } else {
+                $(this).removeClass('input-error');
             }
-
         });
+        // fields validation
 
-        // previous step
-        $('.f1 .btn-previous').on('click', function () {
-            // navigation steps / progress steps
-            var current_active_step = $(this).parents('.f1').find('.f1-step.active');
-            var progress_line = $(this).parents('.f1').find('.f1-progress-line');
-
-            $(this).parents('fieldset').fadeOut(400, function () {
+        if (next_step) {
+            parent_fieldset.fadeOut(400, function () {
                 // change icons
-                current_active_step.removeClass('active').prev().removeClass('activated').addClass('active');
+                current_active_step.removeClass('active').addClass('activated').next().addClass('active');
                 // progress bar
-                bar_progress(progress_line, 'left');
-                // show previous step
-                $(this).prev().fadeIn();
+                bar_progress(progress_line, 'right');
+                // show next step
+                $(this).next().fadeIn();
                 // scroll window to beginning of the form
                 scroll_to_class($('.f1'), 20);
             });
-        });
-
-        // submit
-        $('.f1').on('submit', function (e) {
-
-            // fields validation
-            $(this).find('input[type="text"], input[type="password"], textarea').each(function () {
-                if ($(this).val() == "") {
-                    e.preventDefault();
-                    $(this).addClass('input-error');
-                } else {
-                    $(this).removeClass('input-error');
-                }
-            });
-            // fields validation
-
-        });
+        }
 
     });
+
+    // previous step
+    $('.f1 .btn-previous').on('click', function () {
+        // navigation steps / progress steps
+        var current_active_step = $(this).parents('.f1').find('.f1-step.active');
+        var progress_line = $(this).parents('.f1').find('.f1-progress-line');
+
+        $(this).parents('fieldset').fadeOut(400, function () {
+            // change icons
+            current_active_step.removeClass('active').prev().removeClass('activated').addClass('active');
+            // progress bar
+            bar_progress(progress_line, 'left');
+            // show previous step
+            $(this).prev().fadeIn();
+            // scroll window to beginning of the form
+            scroll_to_class($('.f1'), 20);
+        });
+    });
+
+    // submit
+    $('.f1').on('submit', function (e) {
+
+        // fields validation
+        $(this).find('input[type="text"], input[type="password"], textarea').each(function () {
+            if ($(this).val() == "") {
+                e.preventDefault();
+                $(this).addClass('input-error');
+            } else {
+                $(this).removeClass('input-error');
+            }
+        });
+        // fields validation
+
+    });
+
     var HejriDate = new Intl.DateTimeFormat('ar-FR-u-ca-islamicc', {
             day: 'numeric',
             month: 'long',
@@ -208,4 +187,17 @@ jQuery(document).ready(function ($) {
     $('.dash-sidebar .dropdown').on('hide.bs.dropdown', function () {
         $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
     });
-});
+    // Fixed menu on scroll
+
+    $(window).scroll(function () {
+        var sticky = $('.dash-sidebar'),
+            scroll = $(window).scrollTop();
+
+        if (scroll >= 100) sticky.addClass('fixed');
+        else sticky.removeClass('fixed');
+    });
+
+    $('.dash-sidebar').animate({
+        scrollTop: $(".dash-sidebar li.active").offset().top - 120
+    }, 2000);
+})(jQuery);
